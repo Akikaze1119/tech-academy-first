734fcff5f1bcd67df7c20e4c90e15f7c22372b87
Satomi Nagano / Akikaze1119 <132441773+Akikaze1119@users.noreply.github.com>
2024-10-18 17:50:42 -0700

    feat: create contact section

---
+++ src/containers/Contact.tsx
@@ -0,0 +1,10 @@
+export default function Contact() {
+  return (
+    <section className='contact container'>
+      <div className='contents_width'>
+        <p>24時間オンライン予約はこちら</p>
+        <button>ご予約</button>
+      </div>
+    </section>
+  );
+}
